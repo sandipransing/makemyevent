14ee2dcb9aa30a053c82d6ba0f2c4e39e9ea83a8
Sandip Ransing <sandip@joshsoftware.com>
2012-10-13 10:25:00 +0530

    omniauth twitter login added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user, :user_signed_in?, :get_idea_quote
+
+  private  
+  def current_user  
+    @current_user ||= User.where(:id => session[:user_id]).first if session[:user_id]  
+  end
+
+  def user_signed_in?
+    current_user 
+  end
+
+  def authenticate_user!
+    unless current_user
+      flash[:error] = 'You need to sign in before accessing this page!'
+      redirect_to root_url
+    end
+  end    
+
+  def invalid_url!
+    flash[:error] = 'Invalid URL !!!'
+    redirect_to root_url and return
+  end
 end
