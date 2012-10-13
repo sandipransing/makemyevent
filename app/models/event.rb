4ffbfdcac417b42d7d37f84de608371cd4cfca12
Kiran Chaudhari <kiran@joshsoftware.com>
2012-10-13 10:57:11 +0530

    Added show page for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,9 +14,10 @@ class Event
   field :start_date, type: Time
   field :end_date, type: Time
   field :published, type: Boolean, default: false
+  field :featured, type: Boolean, default: false
 
-  has_mongoid_attached_file :logo
+  has_mongoid_attached_file :logo, :styles => { :small => "50>", :medium => "100>", :large => "200>" }
 
-  validates_presence_of :name, :short_desc, :organizer_name, :organizer_mobile_no, :location#, :start_date, :end_date
+  validates_presence_of :name, :short_desc, :organizer_name, :organizer_mobile_no, :location, :start_date, :end_date
 
 end
