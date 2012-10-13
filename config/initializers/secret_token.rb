b46f05482d42fd799e2108a0264ed22f667b753d
Sandip Ransing <sandip@joshsoftware.com>
2012-10-13 06:57:09 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeMyEvent::Application.config.secret_token = 'c335884f8aafb6171b7c67ef0619abc40da2f78afced2c337670a58c6fba1c8cbce37080febac3a50b2936d7df515bf0424f7f6e9a1aa87550683c2f4ec2eb3d'
