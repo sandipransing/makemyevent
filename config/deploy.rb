2b13e02815f1f6f980547dde785bd291454dd7b3
Shailesh Patil <shailesh@joshsoftware.com>
2012-10-14 09:18:24 +0530

    added the tmp folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :user, 'root'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/mongoid.yml', 'log', 'public/system']
+set :shared_paths, ['config/mongoid.yml', 'log', 'public/system', 'tmp']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
@@ -44,8 +44,10 @@ task :setup => :environment do
 
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+  
+  queue! %[mkdir -p "#{deploy_to}/shared/tmp"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp"]
 
-  queue! %[mkdir -p "#{deploy_to}/shared/public"]
   queue! %[mkdir -p "#{deploy_to}/shared/public/system"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/public"]
 
